c2122db7f3bd37bc94fc334a0fdd453bf423dd8f
farhadhossen <imfarhad01@gmail.com>
2021-11-08 12:16:22 +0600

    image selection from camera and gallery is done

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,44 @@
+
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
-  final count = 0.obs;
+  var selectedImagePath = ''.obs;
+  var selectedImageSize = ''.obs;
+
+  void getImage(ImageSource imageSource) async {
+
+    final pickedFile = await ImagePicker().pickImage(source: imageSource);
+
+    if(pickedFile!=null){
+      selectedImagePath.value = pickedFile.path;
+      selectedImageSize.value = (File(selectedImagePath.value).lengthSync()/1024/1024).toStringAsFixed(2)+" Mb";
+
+    }else{
+      Get.snackbar("Error", "No Image selected",
+      snackPosition: SnackPosition.BOTTOM,
+      backgroundColor: Colors.red,
+      colorText: Colors.white);
+    }
+
+  }
+
   @override
   void onInit() {
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+
 
   @override
-  void onClose() {}
-  void increment() => count.value++;
+  void onClose() {
+
+  }
+
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../controllers/home_controller.dart';
+import 'dart:io';
 
 class HomeView extends GetView<HomeController> {
   @override
@@ -13,9 +15,37 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              Obx(()=>controller.selectedImagePath.value==''?
+                  Text('Select immage from camera/gallery',
+                  style: TextStyle(fontSize: 20),
+                  )
+                  : Image.file(File(controller.selectedImagePath.value))
+              ),
+
+              SizedBox(height: 10,),
+              Obx(()=>Text(controller.selectedImageSize.value==''?'':
+              controller.selectedImageSize.value,
+              style: TextStyle(fontSize: 20),
+              ),
+              ),
+
+              RaisedButton(onPressed: (){
+                controller.getImage(ImageSource.camera);
+              },
+                child: Text("Camera"),
+              ),
+              RaisedButton(onPressed: (){
+                controller.getImage(ImageSource.gallery);
+              },
+              child: Text("Gallery"),
+              ),
+            ],
+          ),
         ),
       ),
     );
